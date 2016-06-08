af8ebb949ae0b2311813c18b538f30b1f42681fe
ahmed <ahmedmatem@gmail.com>
2016-06-08 20:30:18 +0100

    Set connection string name

--- Data/PrimusFlex.Data/ApplicationDbContext.cs
+++ Data/PrimusFlex.Data/ApplicationDbContext.cs
@@ -12,7 +12,7 @@
     public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
     {
         public ApplicationDbContext()
-            : base("DefaultConnection", throwIfV1Schema: false)
+            : base("PrimusFlexConnectionString", throwIfV1Schema: false)
         {
         }
 
